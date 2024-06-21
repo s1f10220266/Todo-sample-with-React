a4c2ae8562fe7341193e224985d4e095924bc7f6
No.eito <s1f102202662@iniad.org>
2024-06-22 01:42:44 +0900

    ToDo追加機能を実装、CSS適応

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import TodoInput from "./TodoInput"
 
 function App() {
   return (
     <div className="App">
       <div className="App-title">Todoアプリ</div>
-      <div className="App-content">Prpject gets Started.</div>
+      <div className="App-content">
+        <TodoInput />
+      </div>
     </div>
   );
 }
